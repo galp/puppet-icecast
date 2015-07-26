a0e682fe4aeee5802a7d24ead2761b37ed3ed170
ag <ag@kiben.net>
2015-07-26 23:05:05 +0100

    initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,28 @@
+class icecast (
+  $source_pass = 'icecast',
+  $admin_pass  = 'icecast',
+  $relay_pass  = 'icecast',
+  $location    = 'Wonderland',
+  $port        = '8000',
+)
+{
+  package {'icecast2':
+    ensure  => installed,
+  }
+  service {'icecast2':
+    ensure  => running,
+    status  => 'pgrep icecast',
+    require => Package['icecast2'],
+  }
+  file {'/etc/icecast2/icecast.xml':
+    ensure  => present,
+    content => template("${module_name}/icecast_xml.erb"),
+    notify  => Service['icecast2']
+  }
+  file {'/etc/default/icecast2':
+    ensure  => present,
+    content => "ENABLE=true",
+    notify  => Service['icecast2']
+  }
+
+}
